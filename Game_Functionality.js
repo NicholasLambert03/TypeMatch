15d8f467ee7629e2bf4f60158db8f9c2b48addce
NicholasLambert03 <nicholas@spunow.co.uk>
2025-05-07 15:16:16 +0100

    Type Matching improved

--- Game_Functionality.js
+++ Game_Functionality.js
@@ -1,105 +1,100 @@
 const gameSetup = async()=> {
-     const errorHandling = (error) =>{
-         
-     };
-     // Get Random Pokémon
-     let pokemon;
-     try{
-         pokemon = await getRandomPokemon();
-         createPokemonCard(pokemon)
-         typesToMatch = []
-         for (let data of pokemon.types){
-             typesToMatch.push(data.type.name)
-             }
-     }
-     catch (error){
-         console.log(`Error in game setup ${error}`)
-         pokemonCardContainer.innerText = `Sorry Ash, the pokémon is in another castle :( \nplease refresh!`
-     }
-     populateDropdown()
- }
-
- const guessHandling = async(guess)=>{
-    console.log(`Submitted ${guess}`)
-    try{``
-        const pokemon = await getPokemonById(guess)
-        if(pokemon === null){
-            console.log(`Pokémon not found`)
-            return false
-        }
-        //Update card with guessed Pokémon or create new card if it doesn't exist
-
-        //Create versus text
-        if(pokemonCardContainer.querySelectorAll('#versus-text').length === 0){
-            showVersusText()
-        }
-        
-        if(pokemonCardContainer.querySelectorAll('#pokemon-card').length >1){
-            updatePokemonCard(pokemon,1)
-        }
-        else{
-            createPokemonCard(pokemon)
-        }
-        for(let data of pokemon.types){
-            const url = data.type.url;
-            typeData = await getTypeData(url)
-        }   
-    }
-    catch(error){
-        console.log(`Pokémon not found ${error}`)
-        return false
-    }
-    if(typesToMatch ===null){
-        console.log('Types to match not loaded yet')
-        return false
-    }
-    return checkEffectiveness(typeData.damage_relations,typesToMatch)
+    await setupNewCard()
+    populateDropdown()
 }
 
-const checkEffectiveness = (guessedTypeRelations,targetTypes) =>{
+const setupNewCard = async()=>{
+   let pokemon;
+    try{
+        pokemon = await getRandomPokemon();
+        createPokemonCard(pokemon)
+        typesToMatch = []
+        for (let data of pokemon.types){
+            typesToMatch.push(data.type.name)
+            }
+    }
+    catch (error){
+        console.log(`Error in game setup ${error}`)
+        pokemonCardContainer.innerText = `Sorry Ash, the pokémon is in another castle :( \nplease refresh!`
+    }
+}
+
+const guessHandling = async(guess)=>{
+   console.log(`Submitted ${guess}`)
+   try{``
+       const pokemon = await getPokemonById(guess)
+       if(pokemon === null){
+           console.log(`Pokémon not found`)
+           return false
+       }
+       //Update card with guessed Pokémon or create new card if it doesn't exist
+
+       //Create versus text
+       if(pokemonCardContainer.querySelectorAll('#versus-text').length === 0){
+           showVersusText()
+       }
+       
+       if(pokemonCardContainer.querySelectorAll('#pokemon-card').length >1){
+           updatePokemonCard(pokemon,1)
+       }
+       else{
+           createPokemonCard(pokemon)
+       }
+       let effectiveness = 0;
+       for(let data of pokemon.types){
+           const url = data.type.url;
+           typeData = await getTypeData(url)
+           effectiveness = Math.max(effectiveness,checkEffectiveness(typeData.damage_relations,typesToMatch))
+       }   
+       return effectiveness >=2;
+   }
+   catch(error){
+       console.log(`Pokémon not found ${error}`)
+       return false
+   }
+}
+
+const checkEffectiveness = (guessedTypeRelations,typeToBeat) =>{
     let multiplier = 1; // Initialize multiplier to 1
-    targetTypes.forEach(targetType => {
-        let ne =  guessedTypeRelations.no_damage_to.filter(guessedType => guessedType.name === targetType).length;
-        let he = guessedTypeRelations.half_damage_to.filter(guessedType => guessedType.name === targetType).length;
-        let se = guessedTypeRelations.double_damage_to.filter(guessedType => guessedType.name === targetType).length;
-        if(ne > 0){
-            multiplier *= 0; // No damage
-            console.log(`No damage to ${targetType}`)
-        }
-        else if(he > 0){
-            multiplier *= 0.5*he; // Half damage
-            console.log(`Half damage to ${targetType}`)
-        }
-        else if(se > 0){
-            multiplier *= 2*se; // Double damage
-            console.log(`Double damage to ${targetType}`)
-        }
-    })
-    console.log(`Multiplier: ${multiplier}`)
-    return multiplier>=2;
- }
- 
+    filt = (t) => typeToBeat.includes(t.name)
+    let ne = guessedTypeRelations.no_damage_to.filter(filt).length;
+    let he = guessedTypeRelations.half_damage_to.filter(filt).length;
+    let se = guessedTypeRelations.double_damage_to.filter(filt).length;
+    if(ne > 0){
+        multiplier *= 0; // No damage
+    }
+    else if(he > 0){
+        multiplier *= 0.5*he; // Half damage
+    }
+    else if(se > 0){
+        multiplier *= 2*se; // Double damage
+    }
+
+   console.log(`Multiplier: ${multiplier}`)
+   return multiplier;
+}
+
 const resultHandling = (result) =>{
-    showResultText(result)
+   showResultText(result)
 }
 
 //Form Handling
 
 const form = document.querySelector('form')
 form.addEventListener('submit',async(event)=>{
-    event.preventDefault()
-    const searchBox = document.querySelector('#search-box')
-    const result = await guessHandling(searchBox.value.toLowerCase());
-    searchBox.value=""
-    resultHandling(result)
+   event.preventDefault()
+   const searchBox = document.querySelector('#search-box')
+   const result = await guessHandling(searchBox.value.toLowerCase());
+   searchBox.value=""
+   resultHandling(result)
 })
 
 //Restart button handling
 const restartButton = document.querySelector('#restart')
 restartButton.addEventListener('click',(event)=>{
-    typesToMatch = null
-    resetScreen()
-    gameSetup()
+   typesToMatch = null
+   resetScreen()
+   setupNewCard()
 
 })
 
